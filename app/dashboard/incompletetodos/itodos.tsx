7b0101a9fc1c11dd178f860bb2b5794df4734915
chengivan <chengoleivan@gmail.com>
2024-05-13 03:25:11 +0600

    Added todo data to page

--- app/dashboard/incompletetodos/itodos.tsx
+++ app/dashboard/incompletetodos/itodos.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default async function getIncompleteTodos() {
+export default async function GetIncompleteTodos() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   const supabase = createClient(supabaseUrl!, supabaseKey!);
@@ -23,6 +23,15 @@ export default async function getIncompleteTodos() {
   }
 
   return(
-    <div>I can see the page.</div>
+    <div>I can see the page.
+        {data?.map((todo) => (
+        <div key={todo.todoId}>
+          <h2>{todo.todoname}</h2>
+          <p>{todo.tododescription}</p>
+          <p>{todo.completed ? 'Completed' : 'Not completed'}</p>
+          <p>Added on: {new Date(todo.timeadded).toLocaleString()}</p>
+        </div>
+      ))}
+    </div>
   )
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 import { createClient } from "@/utils/supabase/server";
-import Todoform from './addtodos/todoform';
-import Link from 'next/link';
+import Todoform from "./addtodos/todoform";
+import Link from "next/link";
+import GetIncompleteTodos from "./incompletetodos/itodos";
 
 export default async function page() {
-    const supabase = createClient();
+  const supabase = createClient();
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return ( user?
-    <Todoform /> :
-    <div>You need to <Link href="/login">Sign In</Link></div>)
+  return user ? (
+    <div>
+      <Todoform />
+      <GetIncompleteTodos />
+    </div>
+  ) : (
+    <div>
+      You need to <Link href="/login">Sign In</Link>
+    </div>
+  );
 }
