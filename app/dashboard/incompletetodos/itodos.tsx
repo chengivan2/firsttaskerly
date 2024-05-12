8814632d54c15c29ee6f339284ecf9a10e983c52
chengivan <chengoleivan@gmail.com>
2024-05-13 03:50:47 +0600

    Fix 2

--- app/dashboard/incompletetodos/itodos.tsx
+++ app/dashboard/incompletetodos/itodos.tsx
@@ -1,20 +1,22 @@
 import { createClient } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
 
 export default async function GetIncompleteTodos() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   const supabase = createClient(supabaseUrl!, supabaseKey!);
+  
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  const taskerly_userId = user?.id;
+  const taskerly_userId = await user?.id;
   let { data, error } = await supabase
     .from("current_todos")
     .select("*")
     .eq("todouserId", taskerly_userId)
     .eq("completed", false);
 
-    console.log(await data)
+  console.log(await data);
 
   if (error) {
     console.error("Error fetching todos:", error);
@@ -22,18 +24,21 @@ export default async function GetIncompleteTodos() {
     console.log("Fetched todos:", data);
   }
 
-  return(
-    <div>I can see the page.
-        {data?
+  return (
+    <div>
+      I can see the page.
+      {data ? (
         data?.map((todo) => (
-            <div key={todo.todoId}>
-              <h2>{todo.todoname}</h2>
-              <p>{todo.tododescription}</p>
-              <p>{todo.completed ? 'Completed' : 'Not completed'}</p>
-              <p>Added on: {new Date(todo.timeadded).toLocaleString()}</p>
-            </div>
-          )):
-          <p>Go pee yourself!</p>}
+          <div key={todo.todoId}>
+            <h2>{todo.todoname}</h2>
+            <p>{todo.tododescription}</p>
+            <p>{todo.completed ? "Completed" : "Not completed"}</p>
+            <p>Added on: {new Date(todo.timeadded).toLocaleString()}</p>
+          </div>
+        ))
+      ) : (
+        <p>Go pee yourself!</p>
+      )}
     </div>
-  )
+  );
 }
