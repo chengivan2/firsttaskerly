ab9579a6fadacc1abd06d24da510cf84e743d5fa
chengivan <chengoleivan@gmail.com>
2024-05-13 02:00:09 +0600

    Added log out button to dashboard

--- app/dashboard/addtodos/todoform.tsx
+++ app/dashboard/addtodos/todoform.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default function Todoform() {
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
   return (
-    <div>This page will showthe todo form if you are signed in.</div>
-  )
+    <div>
+      <div>This page will show the todo form if you are signed in.</div>
+      <form action={signOut}>
+        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+          Logout
+        </button>
+      </form>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = "https://firsttaskerly.vercel.app"
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
