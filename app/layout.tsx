b03e6d405afc668baf318756ce0f95bd35df687a
chengivan <chengoleivan@gmail.com>
2024-05-12 21:54:23 +0600

    Changed title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "First Taskerly",
+  description: "The fastest way to manage your tasks",
 };
 
 export default function RootLayout({
